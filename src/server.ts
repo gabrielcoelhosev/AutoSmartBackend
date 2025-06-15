c1072a1dfbe92e6da0add312d0c5fdb074fa2332
Gabriel Coelho <gabrielcoelhosev.dev@gmail.com>
2025-06-14 23:48:53 -0300

    feat: Logs Terminal

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,19 @@ import router from './routes';
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:4200'
+    origin: 'http://localhost:5173'
 }))
+
+app.use((req, res, next) => {
+    const timestamp = new Date().toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo' });
+    const ip = req.ip || req.connection.remoteAddress;
+    const method = req.method;
+    const url = req.originalUrl;
+    const userAgent = req.headers['user-agent'];
+
+    console.log(`[${timestamp}] - IP: ${ip} - Método: ${method} - URL: ${url} - User-Agent: ${userAgent}`);
+    next();
+});
 
 app.use(express.json());
 app.use(router);
